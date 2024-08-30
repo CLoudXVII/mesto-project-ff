cda093ed647db543929581a5367326e8421caec3
CLoudXVII <cloudofficial@bk.ru>
2024-08-30 11:04:00 +0300

    script for card creation and removal

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,16 @@
-// @todo: Темплейт карточки
+cardTemplate = document.querySelector('#card-template').content;
+cardList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+cardRemove = (evt) => evt.target.closest('.card').remove();
 
-// @todo: Функция создания карточки
+function cardAdd (card) {
+  cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__title').textContent = card.name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', cardRemove);
+  cardList.append(cardElement);
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+for (let i = 0; i < initialCards.length; i++) {
+  cardAdd(initialCards[i])
+}

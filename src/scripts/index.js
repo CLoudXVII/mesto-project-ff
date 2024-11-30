5418a283fa490789597795892f17848d12336eee
CLoudXVII <cloudofficial@bk.ru>
2024-11-30 16:26:20 +0300

    added an ability to open and close the profile edit popup with esc, click on the overlay and close button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,8 @@ import { initialCards } from "./cards";
 
 const cardTemplate = document.querySelector('#card-template').content;
 const cardList = document.querySelector('.places__list');
+const profileEditButton = document.querySelector('.profile__edit-button');
+const profileEditPopup = document.querySelector('.popup_type_edit');
 
 function cardRemove(evt) {
   evt.target.closest('.card').remove();
@@ -17,4 +19,25 @@ function cardCreate (card, removalFunc) {
   return cardElement
 }
 
+function handlePopupClose(evt) {
+  if ((evt.target.classList.contains('popup')) || (evt.target.classList.contains('popup__close'))) {
+    evt.target.closest('.popup').classList.remove('popup_is-opened');
+    profileEditPopup.removeEventListener('click', handlePopupClose)
+    document.removeEventListener('keydown', handlePopupEscClose);
+  }
+}
+
+function handlePopupEscClose(evt) {
+  if (evt.key === 'Escape') {
+    profileEditPopup.classList.remove('popup_is-opened');
+    document.removeEventListener('keydown', handlePopupEscClose);
+  }
+}
+
 initialCards.forEach(element => cardList.append(cardCreate(element, cardRemove)));
+
+profileEditButton.addEventListener('click', function() {
+  profileEditPopup.classList.add('popup_is-opened');
+  profileEditPopup.addEventListener('click', handlePopupClose)
+  document.addEventListener('keydown', handlePopupEscClose);
+})
